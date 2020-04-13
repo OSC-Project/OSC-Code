134a11a56cd325475c449220e18f4682447a2801
Alex <alex.o'neill@uconn.edu>
2020-04-13 12:16:58 -0400

    Added FP
    009 is a false positive eval we have discovered and is now implemented

---
+++ benchmark/BenchmarkTest009.js
@@ -0,0 +1,25 @@
+//category: test-number: vulnerability:false cwe: cve:none
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req,res){
+        res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.post('/', function(req, res){
+        let param = req.body.name_field;
+        let x = param + " executed";
+        try {
+          x = eval('throw "STOP"; (function () { return (' + param + '); })()');
+        } catch (e) {
+          x = param + " was used"
+        }
+        res.send(x)
+});
+
+app.listen(process.env.port || 8080);
+console.log('Running on port 8080');
