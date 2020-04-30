c3115c620f8650f8170aa47e49cf78f4fb4f8f8b
Alex <alex.o'neill@uconn.edu>
2020-04-30 16:16:29 -0400

    Scorecard Update
    should loop easier, once I know if the results from different quiries are in one csv file or multiple I can finish the score card creator and move on to integration

--- benchmark/scorecard_LGTM.py
+++ benchmark/scorecard_LGTM.py
@@ -1,66 +1,17 @@
 import csv
 
 benchmarks = []
+#cwes = [94, 89, 78]
+cwes=[94]
 
-truepos = 0
-falsepos = 0
-trueneg = 0
-falseneg = 0
-total = 0
-
-cwe_used = 0
-
-with open('Benchmark Test Results.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    for row in readCSV:
-        #print(row)
-        if ('File Name' not in row[0]):
-            benchmarks.append(row)
-
+total_TP = 0
+total_FN = 0
+total_TN = 0
+total_FP = 0
+total_total = 0
 #print(benchmarks)
-try:
-    with open('output.csv') as csvfile:
-        readCSV = csv.reader(csvfile, delimiter=',')
-        for row in readCSV:
-            for line in benchmarks:
-                 if line[0] in row[3]:
-                     cwe_used = line[1]
-                     line[4] = "True"
-                     total += 1
-                     if line[2] == "TRUE":
-                         truepos += 1
-                     else:
-                         falsepos += 1
-except IOError:
-    print("'output.csv' file is found or is not accessible")
-    raise
-
-
-for line in benchmarks:
-    if line[1] == cwe_used:
-        if line[4] == "FALSE" and line[2] == "TRUE":
-            falseneg += 1
-        else:
-            pass
-    else:
-        trueneg += 1
-
-# print(truepos)
-# print(falsepos)
-# print(falseneg)
-# print(trueneg)
-# print(total)
-
 # name of csv file
 filename = "scorecard.csv"
-
-tpr = truepos / (truepos+falseneg)
-fpr = falsepos / (falsepos + trueneg)
-score = tpr - fpr
-
-results = [cwe_used, truepos, falseneg, trueneg, falsepos, total, tpr, fpr, score]
-results = [str(i) for i in results]
-print(results)
 # writing to csv file
 with open(filename, 'w') as csvfile:
     # creating a csv writer object
@@ -68,6 +19,61 @@ with open(filename, 'w') as csvfile:
 
     # writing the fields
     csvwriter.writerow(['CWE', 'TP', 'FN','TN','FP','Total', 'TPR', 'FPR', 'Score'])
+    #print(benchmarks)
+    for cwe in cwes:
+        truepos = 0
+        falsepos = 0
+        trueneg = 0
+        falseneg = 0
+        total = 0
+        with open('Benchmark Test Results.csv') as csvfile:
+            readCSV = csv.reader(csvfile, delimiter=',')
+            for row in readCSV:
+                #print(row)
+                if ('File Name' not in row[0]):
+                    benchmarks.append(row)
+        try:
+            with open('output.csv') as csvfile:
+                readCSV = csv.reader(csvfile, delimiter=',')
+                for row in readCSV:
+                    for line in benchmarks:
+                         if line[0] in row[3]:
+                             #cwe_used = line[1]
+                             line[4] = "TRUE"
+                             total += 1
+                             if line[2] == "TRUE":
+                                 truepos += 1
+                             else:
+                                 falsepos += 1
+        except IOError:
+            print("'output.csv' file is found or is not accessible")
+            raise
 
-    # writing the data rows
-    csvwriter.writerow(results)
+
+        for line in benchmarks:
+            if line[1] == str(cwe):
+                if line[4] == "FALSE" and line[2] == "TRUE":
+                    falseneg += 1
+                else:
+                    pass
+            else:
+                trueneg += 1
+        tpr = truepos / (truepos+falseneg)
+        fpr = falsepos / (falsepos + trueneg)
+        score = tpr - fpr
+
+        results = [cwe, truepos, falseneg, trueneg, falsepos, total, tpr, fpr, score]
+        results = [str(i) for i in results]
+        #print(results)
+
+        total_TP += truepos
+        total_FN += falseneg
+        total_TN += trueneg
+        total_FP += falsepos
+        total_total += total
+        # writing the data rows
+        csvwriter.writerow(results)
+    avg_tpr = total_TP / (total_TP+total_FN)
+    avg_fpr = total_FP / (total_FP + total_TN)
+    avg_score = avg_tpr - avg_fpr
+    csvwriter.writerow(["", "Total TP:\n" + str(total_TP), "Total FN:\n" + str(total_FN), "Total TN:\n" + str(total_TN), "Total FP:\n" + str(total_FP), "Total Test Cases:\n" + str(total_total), "Average TPR:\n" + str(avg_tpr), "Average FPR:\n" + str(avg_fpr), "Average Score:\n" + str(avg_score)])
