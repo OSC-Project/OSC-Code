4e6d14c5fde40047284161fdc720cfdbdbbc1baa
Alex <alex.o'neill@uconn.edu>
2020-05-17 18:23:48 -0400

    Name updates
    default queries are now under LGTM name, custom is under LGTM++

--- Tools/LGTM/scorecard.py
+++ Tools/LGTM/scorecard.py
@@ -10,20 +10,16 @@ def makeGraph(fpr=0, tpr=0, title="Scorecard Graph"):
     x = fpr[0]
     y = tpr[0]
     plt.plot(x, y, marker='o', markerfacecolor='red', markersize=12)
-    red_patch = mpatches.Patch(color='red', label='Custom Query')
+    red_patch = mpatches.Patch(color='red', label='LGTM++')
     x = fpr[1]
     y = tpr[1]
     plt.plot(x, y, marker='o', markerfacecolor='blue', markersize=12)
-    blue_patch = mpatches.Patch(color='blue', label='Default Security Query')
-    x = fpr[2]
-    y = tpr[2]
-    plt.plot(x, y, marker='o', markerfacecolor='orange', markersize=12)
-    orange_patch = mpatches.Patch(color='orange', label='Default Eval Query')
+    blue_patch = mpatches.Patch(color='blue', label='LGTM')
     x=0.66
     y=1
     plt.plot(x, y, marker='o', markerfacecolor='green', markersize=12)
     green_patch = mpatches.Patch(color='green', label='Sink Finder')
-    plt.legend(handles=[red_patch, blue_patch, green_patch, orange_patch], loc=4)
+    plt.legend(handles=[red_patch, blue_patch, green_patch], loc=4)
     plt.plot([0,1], [0,1], color='black', linestyle='dashed', linewidth = 3)
     plt.xlabel('false positive rate')
     plt.ylabel('true positive rate')
@@ -36,17 +32,16 @@ map = {"Code injection" : 94}
 #print(benchmarks)
 xs=[]
 ys=[]
-for file in ['customCodeInjection2', '_CodeInjection', '_Eval']:
+for file in ['LGTM++', 'LGTM']:
     benchmarks = []
     total_TP = 0
     total_FN = 0
     total_TN = 0
     total_FP = 0
     total_total = 0
-    with open(file+' over Comcast50+'+'.csv', 'w') as csvfile:
+    with open(file+' over Packages'+'.csv', 'w') as csvfile:
         # creating a csv writer object
         csvwriter = csv.writer(csvfile)
-
         # writing the fields
         csvwriter.writerow(['CWE', 'TP', 'FN','TN','FP','Total', 'TPR', 'FPR', 'Score'])
         #print(benchmarks)
@@ -66,36 +61,68 @@ for file in ['customCodeInjection2', '_CodeInjection', '_Eval']:
                 row.append("FALSE")
 
             for filename in os.listdir("./Query_Results"):
-                if file in filename:
-                    with open('./Query_Results/'+filename) as csvfile:
-                        try:
-                            csv.Sniffer().has_header(csvfile.read(1))
-                            readCSV = csv.reader(csvfile, delimiter=',')
-                            for row in readCSV:
+                if file == 'LGTM++':
+                    if 'custom' in filename:
+                        with open('./Query_Results/'+filename) as csvfile:
+                            try:
+                                csv.Sniffer().has_header(csvfile.read(1))
+                                readCSV = csv.reader(csvfile, delimiter=',')
+                                for row in readCSV:
+                                    flag = 0
+                                    for line in benchmarks:
+                                         if line[1] in row[4] and str(line[4]) == str(row[5]):
+                                             line[5] = "TRUE"
+                                             truepos += 1
+                                             flag = 1
+                                    if (flag == 1):
+                                        flag = 0
+                                    else:
+                                        falsepos += 1
+                                        flag = 0
+                            except:
                                 flag = 0
                                 for line in benchmarks:
-                                     if line[1] in row[4] and str(line[4]) == str(row[5]):
-                                         line[5] = "TRUE"
-                                         truepos += 1
-                                         flag = 1
-                                if (flag == 1):
+                                    if line[0].replace(".", "-") in filename:
+                                        #depot-0.1.6...    vs depot-0-1-6...
+                                        if line[5] == "FALSE":
+                                            falseneg += 1
+                                            flag = 1
+                                if flag == 1:
                                     flag = 0
                                 else:
-                                    falsepos += 1
+                                    trueneg += 1
                                     flag = 0
-                        except:
-                            flag = 0
-                            for line in benchmarks:
-                                if line[0].replace(".", "-") in filename:
-                                    #depot-0.1.6...    vs depot-0-1-6...
-                                    if line[5] == "FALSE":
-                                        falseneg += 1
-                                        flag = 1
-                            if flag == 1:
+                else:
+                    if '_Code' in filename or '_Eval' in filename:
+                        with open('./Query_Results/'+filename) as csvfile:
+                            try:
+                                csv.Sniffer().has_header(csvfile.read(1))
+                                readCSV = csv.reader(csvfile, delimiter=',')
+                                for row in readCSV:
+                                    flag = 0
+                                    for line in benchmarks:
+                                         if line[1] in row[4] and str(line[4]) == str(row[5]):
+                                             line[5] = "TRUE"
+                                             truepos += 1
+                                             flag = 1
+                                    if (flag == 1):
+                                        flag = 0
+                                    else:
+                                        falsepos += 1
+                                        flag = 0
+                            except:
                                 flag = 0
-                            else:
-                                trueneg += 1
-                                flag = 0
+                                for line in benchmarks:
+                                    if line[0].replace(".", "-") in filename:
+                                        #depot-0.1.6...    vs depot-0-1-6...
+                                        if line[5] == "FALSE":
+                                            falseneg += 1
+                                            flag = 1
+                                if flag == 1:
+                                    flag = 0
+                                else:
+                                    trueneg += 1
+                                    flag = 0
             total = truepos + trueneg + falsepos + falseneg
             tpr = truepos / (truepos+falseneg)
             fpr = falsepos / (falsepos + trueneg)
@@ -118,4 +145,4 @@ for file in ['customCodeInjection2', '_CodeInjection', '_Eval']:
         csvwriter.writerow(["", "Total TP:\n" + str(total_TP), "Total FN:\n" + str(total_FN), "Total TN:\n" + str(total_TN), "Total FP:\n" + str(total_FP), "Total Test Cases:\n" + str(total_total), "Average TPR:\n" + str(avg_tpr), "Average FPR:\n" + str(avg_fpr), "Average Score:\n" + str(avg_score)])
         xs.append(avg_fpr)
         ys.append(avg_tpr)
-makeGraph(xs, ys, 'Custom vs Default over Comcast50+')
+makeGraph(xs, ys, 'Tools over Packages')
